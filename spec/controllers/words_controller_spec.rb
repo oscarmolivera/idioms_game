f9c97e17a8cb28d91bfdbe0bb53d25533730032c
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-26 16:01:17 -0400

    Action methods NEW/CREATE WordsController close #9

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
   describe ' GET index' do
-    subject { get :index }
+    before { get :index }
+
     context 'when @words is present' do
       let(:word) { create(:word) }
       it 'assigns @words' do
-        subject
         expect(assigns(:words)).to eq([word])
       end
     end
+
     context 'when no require validations' do
       it 'renders the index template' do
-        subject
         expect(response).to render_template(:index)
       end
     end
