28447feb23a220cd922eccf2712f7a335809f36f
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-29 16:46:05 -0400

    Words Association fix

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,5 +2,5 @@ class Word < ApplicationRecord
   belongs_to :language
   
   validates :content, :language, presence: true
-  validates :language, inclusion: { in: LanguageList::COMMON_LANGUAGES.map(&:name) }
+  # validates :language, inclusion: { in: LanguageList::COMMON_LANGUAGES.map(&:name) }
 end
