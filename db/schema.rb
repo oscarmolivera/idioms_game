17df39a8878f5274aecb85292147145a8373b7ed
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-29 15:33:40 -0400

    Adding Language reference to Word Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_191203) do
+ActiveRecord::Schema.define(version: 2020_06_29_193050) do
 
   create_table "languages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 2020_06_29_191203) do
     t.string "language"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "language_id"
+    t.index ["language_id"], name: "index_words_on_language_id"
   end
 
+  add_foreign_key "words", "languages"
 end
