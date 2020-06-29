7ecd2118a926f19db342c4eb1a956aff8dd6c424
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-29 15:35:26 -0400

    Language-Word Association

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,6 @@
 class Word < ApplicationRecord
+  belongs_to :language
+  
   validates :content, :language, presence: true
   validates :language, inclusion: { in: LanguageList::COMMON_LANGUAGES.map(&:name) }
 end
