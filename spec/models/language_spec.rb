bdc9219b199152c068fbe3c7b8ca28ada524da49
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-29 15:28:40 -0400

    Validations and SPECs for Language model close #18

---
+++ spec/models/language_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Language, type: :model do
+  describe 'initial columns' do
+    it { is_expected.to have_db_column(:name) }
+  end
+
+  describe 'validations' do
+    let(:languages) { LanguageList::COMMON_LANGUAGES.map(&:name) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_inclusion_of(:name).in_array(languages) }
+  end
+end
