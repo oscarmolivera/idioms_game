6a9a4219b0572e65f54ce248f669cf1058a6b8ef
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-02 19:51:29 -0400

    Toastr Helper Method and View Implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def toastr_flash_messages
+    flash_messages = []
+    flash.each do |type, message|
+      type = 'success' if type == 'notice'
+      type = 'error' if type == 'alert'
+      type = 'info' if type == 'info'
+      type = 'warning' if type == 'warning'
+      text = "<script>toastr.#{type}('#{message}', '#{type.upcase}', {closeButton:true, progressBar: true});</script>"
+      flash_messages << text.html_safe if message
+    end
+    flash_messages.join('\n').html_safe
+  end
 end
