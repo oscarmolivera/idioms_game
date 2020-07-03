af0978371bb90c87e80895f32525cf8ddb470547
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-03 13:48:00 -0400

    Adding NewUsers / WORDs to SeedFile close #30

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,28 @@ User.create!(
 )
 puts 'My User created!'
 
+4.times do
+  pass = Faker::Internet.password(min_length: 8)
+  User.create!(
+    email: Faker::Internet.email,
+    password: pass,
+    password_confirmation: pass,
+  )
+end
+puts '4 Dummy users created!'
+
 8.times do
   Language.create!(
     name: Faker::Verb.base.capitalize
   )
 end
 puts '8 Dummy Languages created!'
+
+50.times do
+  Word.create!(
+    content: Faker::Lorem.word,
+    language_id: Faker::Number.between(from: 1, to: 8),
+    user_id: Faker::Number.between(from: 1, to: 5)
+  )
+end
+puts '25 Words Created!'
