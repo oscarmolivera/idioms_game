b7f8b514b894b609d4712aa8c9413798a5bd8468
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-03 12:49:02 -0400

    Corrrecting User Assigning ID to Word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -13,7 +13,7 @@ class WordsController < ApplicationController
 
   def create
     @word = Word.new(word_params)
-    @word.user = current_user
+    @word.user_id = current_user.id
     if @word.save
       redirect_to(words_path)
     else
