b4a380f240302094e76c145a4b507ef9e8723725
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-03 11:56:06 -0400

    WordsController SPECs for SignIn Actions close #27

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -19,11 +19,9 @@ RSpec.describe WordsController, type: :controller do
   end
 
   describe 'GET New' do
-    
-    before { get :new }
-    login_user
-
     context 'when user is logged in' do  
+      login_user
+      before { get :new }
       it 'assigns @word' do
         expect(assigns(:word)).to be_a_new(Word)
       end
@@ -31,26 +29,72 @@ RSpec.describe WordsController, type: :controller do
       it 'renders the new template' do
         expect(response).to render_template(:new)
       end
+
+      it 'has a valid page' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when user in not logged in' do
+      before { get :new }
+
+      it 'does not assigns @word' do
+        expect(assigns(:word)).not_to be_a_new(Word)
+      end
+
+      it 'renders the new template' do
+        expect(response[:location]).to redirect_to(new_user_session_path)
+      end
+      
+      it 'has a valid page' do
+        expect(response).to have_http_status(:found) #302
+      end
     end
   end
 
   describe 'POST Create' do
-    context 'when valid params' do
-      let(:language) { create(:language) }
-      let(:params) { { word: { content: 'Home', language_id: language.id } } }
-      subject { post :create, params: params }
+    context 'when user logged in' do
+      login_user
+      context 'when valid params' do
+        let(:language) { create(:language) }
+        let(:params) { { word: { content: 'Home', language_id: language.id } } }
+        subject { post :create, params: params }
+  
+        it 'create a new word' do
+          expect { subject }.to change { Word.count }.by(1)
+        end
 
-      it 'create a new word' do
-        expect { subject }.to change { Word.count }.by(1)
+        it 'has a valid page' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+  
+      context 'when invalid params' do
+        let(:params) { { word: { content: '', language_id: :nil } } }
+        subject { post :create, params: params }
+  
+        it 'does not create a new word' do
+          expect { subject }.not_to (change { Word.count })
+        end
       end
     end
 
-    context 'when invalid params' do
+    context 'when user is not logged in' do
       let(:params) { { word: { content: '', language_id: :nil } } }
       subject { post :create, params: params }
 
       it 'does not create a new word' do
         expect { subject }.not_to (change { Word.count })
+      end
+
+      it 'redirect to login in page' do
+        subject
+        expect(response.location).to redirect_to(new_user_session_path)
+      end
+
+      it 'has a valid page' do
+        subject
+        expect(response).to have_http_status(302)
       end
     end
   end
@@ -72,16 +116,27 @@ RSpec.describe WordsController, type: :controller do
   end
 
   describe 'GET Edit' do
-    before { get :edit, params: params }
     let(:word) { create(:word) }
     let(:params) { { id: word.id} }
+    context 'when user is logged in' do
+      login_user
+      before { get :edit, params: params }
 
-    it 'assigns @word' do
-      expect(assigns(:word)).to eq(word)
+      it 'assigns @word' do
+        expect(assigns(:word)).to eq(word)
+      end
+
+      it 'renders the edit template' do
+        expect(response).to render_template(:edit)
+      end                     
     end
 
-    it 'renders the edit template' do
-      expect(response).to render_template(:edit)
+    context 'when user is not logged in' do
+      before { get :edit, params: params }
+
+      it 'redirect to Login user page' do
+        expect(response.location).to redirect_to(new_user_session_path)
+      end
     end
   end
 
@@ -90,50 +145,67 @@ RSpec.describe WordsController, type: :controller do
     let(:language1) { create(:language) }
     let(:language2) { create(:language) }
 
-    context 'when params are correct' do
-      let(:params) { { id: word.id, word: { content: 'House', language_id: language2.id } } }
-      subject {put :update, params: params}
-      
-      it 'update word' do
-        expect { subject }
-          .to change { word.reload.content }
-          .from(word.content)
-          .to('House')
-          .and change {word.reload.language_id}
-          .from(language1.id)
-          .to(language2.id)
+    context 'when user is logged in' do 
+      context 'and params are correct' do
+        let(:params) { { id: word.id, word: { content: 'House', language_id: language2.id } } }
+        subject {put :update, params: params}
+        
+        it 'update word' do
+          expect { subject }
+            .to change { word.reload.content }
+            .from(word.content)
+            .to('House')
+            .and change {word.reload.language_id}
+            .from(language1.id)
+            .to(language2.id)
+        end
+  
+        it 'update language' do
+          expect { subject }
+            .to change { word.reload.language_id }
+            .from(word.language_id)
+            .to(language2.id)
+        end
       end
-
-      it 'update language' do
-        expect { subject }
-          .to change { word.reload.language_id }
-          .from(word.language_id)
-          .to(language2.id)
+      
+      context 'and params are incorrect' do
+        let(:params) { { id: word.id, word: { content: '', language_id: nil } } }
+  
+        it 'will not update the content' do
+          expect { subject }.not_to change { word.reload.content }
+        end
+  
+        it 'will not update the language' do
+          expect { subject }.not_to change { word.reload.language_id }
+        end
       end
     end
     
-    context 'when params are incorrect' do
-      let(:params) { { id: word.id, word: { content: '', language_id: nil } } }
-
-      it 'will not update the content' do
-        expect { subject }.not_to change { word.reload.content }
-      end
-
-      it 'will not update the language' do
-        expect { subject }.not_to change { word.reload.language_id }
-      end
-    end
   end
 
   describe 'DELETE destroy' do
     let!(:word) { create(:word) }
     let!(:params) { {id: word.id  } }
 
-    context 'when user is permited' do
+    context 'when user is logged in' do
+      login_user
       subject { delete :destroy, params: params}
 
       it 'delete the word' do
         expect { subject }.to change { Word.count }.by(-1)
+      end
+    end
+
+    context 'when user is not logged in' do
+      subject { delete :destroy, params: params}
+
+      it 'does not delete the word' do
+        expect { subject }.not_to change { Word.count }
+      end
+
+      it 'redirect to login in page' do
+        subject
+        expect(response.location).to redirect_to(new_user_session_path)
       end
     end
   end
