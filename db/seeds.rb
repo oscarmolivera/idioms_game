2145cc3078d1ec3894d25295e9a6701c74b43678
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-03 12:33:26 -0400

    Adding MYUser to SeedsFile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 require 'faker'
 
+User.create!(
+  email: 'oscarmolivera@gmail.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+puts 'My User created!'
+
 8.times do
   Language.create!(
     name: Faker::Verb.base.capitalize
