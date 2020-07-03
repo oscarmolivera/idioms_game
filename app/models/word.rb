4e107a63d2d930fe7dbd02ca7a5d969ebf1ab4d8
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-03 12:31:36 -0400

    User/Words Relationship

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,6 @@
 class Word < ApplicationRecord
   belongs_to :language
+  belongs_to :user
   
   validates :content, :language, presence: true
   # validates :language, inclusion: { in: LanguageList::COMMON_LANGUAGES.map(&:name) }
