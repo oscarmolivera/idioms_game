8dd63ba4cf7a1c8315a73ff9281f5de1583aab59
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-05 15:04:31 -0400

    Adding NestedForm in WebPack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
     require("@rails/ujs").start()
     require("turbolinks").start()
     require("@rails/activestorage").start()
-    require("channels")    
+    require("channels")
+    require("packs/nested_form") 
     
     import 'bootstrap';
     import './stylesheets/application.scss';
