d4c449aef79d51b094388f15912722715ca06065
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-06 19:37:43 -0400

    Removing Navbar from T.Rascia JSFIle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,7 @@
     require("turbolinks").start()
     require("@rails/activestorage").start()
     require("channels")
-    require("packs/nested_form") 
-    require("packs/trascia_navbar") 
+    require("packs/nested_form")
     
     import 'bootstrap';
     import './stylesheets/application.scss';
