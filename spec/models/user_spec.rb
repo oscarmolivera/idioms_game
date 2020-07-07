18438fd6714771151cd825b55a791dccea958f85
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-07 12:38:46 -0400

    User Spec-GameAssociations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:words) }
+    it { is_expected.to have_many(:games) }
   end
 end
