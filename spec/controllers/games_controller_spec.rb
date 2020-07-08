31d148da74cb5c8618081c94efe7277f4b5ef20b
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-08 17:59:43 -0400

    GameController #Create Specs

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,30 +3,24 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
 
   describe 'Post #create' do
-    context 'when user is logged in' do
+    context 'when user is signed in' do
       let(:user) { create(:user) }
       let(:game) { create(:game) }
       subject { post :create, params:  { id:game.id }  }
 
       before do
-        sign_in user
+        sign_in(user)
         subject
       end
 
-      it 'redirect to show page (@game)' do
-        expect(response).to redirect_to("/games/#{Game.last.id}")
-      end
-
-      it 'renders the show template' do
-        expect(response).to render_template(:create)
-      end
-
-      it 'HttpResponse is OK' do
-        expect(response).to have_http_status(:ok)
+      it 'redirect to show page (@game)' do #redirects to new game
+         expect(response).to redirect_to("/games/#{Game.last.id}")
+         expect(Game.last.user).to eq(user) # doble checking asosociation.
+        #expect(subject).to redirect_to(user.games.last) --other way
       end
     end
 
-    context 'when user is not logged in' do
+    context 'when user is not signed in' do
       before { get :show, params: { id: 1 } }
 
       it 'redirect to login page' do
@@ -37,7 +31,7 @@ RSpec.describe GamesController, type: :controller do
         expect(response).not_to render_template(:show)
       end
 
-      it do
+      it 'the page show is :found' do
         expect(response).to have_http_status(302)
       end      
     end
