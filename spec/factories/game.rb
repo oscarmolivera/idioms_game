ac1e82a302cc5e7085f4c0fcd651ef7371e2f8d5
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-10 20:16:24 -0400

    SeedFile Update

--- spec/factories/game.rb
+++ spec/factories/game.rb
@@ -3,7 +3,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :game do |g|
-    g.good_answers_count { 7 }
+    g.good_answers_count { Faker::Number.between(from: 1, to: 8) }
     g.bad_answers_count { Faker::Number.between(from: 1, to: 8) }
     user
   end
