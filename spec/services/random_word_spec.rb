772482995695368423d3c569826ed771f07d5140
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-11 14:19:03 -0400

    Service::RamdomWord-Spec #call Words Translations

--- spec/services/random_word_spec.rb
+++ spec/services/random_word_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Words::RandomWord do
-  describe '#call' do 
-    subject { described_class.new.call }
-    let!(:word) { create(:word) }
-    it 'generate a random word from database' do
-      expect(subject).to eq(word)
+  describe '#call' do
+
+    context 'when word has no translations' do
+      subject { described_class.new.call }    
+      let!(:word) { create(:word) }
+
+      it 'does not gives any word' do
+        expect(subject).to eq(nil)
+      end
+    end
+
+    context 'when word has any translations' do
+      subject { described_class.new.call }    
+      let!(:word) { create(:word, :with_translations) }
+
+      it 'does not gives any word' do
+        expect(subject).to eq(word)
+      end
     end
   end
 
