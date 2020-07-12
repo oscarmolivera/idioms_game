d1526678e4d1ed4a3eb8a99bfd0c06e8fcbb62e4
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-12 18:01:14 -0400

    AnswersController #authorize_user close# 60

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, only: %i[create]
+  before_action :authorize_user!, only: %i[create]
 
   def create
     checker = Words::CheckAnswer.new(word, answer, game).call
@@ -24,5 +25,9 @@ class AnswersController < ApplicationController
     return redirect_back(fallback_location: game_path(game), notice: t('game.good_answer')) if checker
 
     redirect_back(fallback_location: game_path(game), alert: t('game.bad_answer'))
+  end
+
+  def authorize_user!
+    authorize game
   end
 end
