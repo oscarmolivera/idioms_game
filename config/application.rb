d6b1388b91dac935cb6f28047bc766eb9003c2fc
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-12 15:28:05 -0400

    Revert "I18N basic Config [EN]"
    This reverts commit eeca0c354d7a73bf5a5205958bf09ad05b227d5f.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,5 @@ module IdiomsGame
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
