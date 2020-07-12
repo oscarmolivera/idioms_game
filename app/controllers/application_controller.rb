80fdc05fa5eb63a51a7f1ca5ff3416f693fce370
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-12 15:29:39 -0400

    I18N basic Config [EN] with App SetLocale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   include Pundit
   include Pagy::Backend
   protect_from_forgery with: :null_session, only: [:create]
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
