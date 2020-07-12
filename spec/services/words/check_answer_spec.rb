7698d9fd0dd1d694a5a1c40336bd26c858b883b9
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-11 23:14:33 -0400

    Services.CheckAnswers-Spec #Call Refactor adding Game

--- spec/services/words/check_answer_spec.rb
+++ spec/services/words/check_answer_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Words::CheckAnswer do
-  let!(:word) { create(:word, :with_translations)}
+  let!(:word) { create(:word, :with_translations) }
+  let!(:game) { create(:game) }
   
   describe '# call' do
-    subject { described_class.new(word, answer).call }
+    subject { described_class.new(word, answer, game).call }
 
     context 'when good answer is provided' do
       let(:answer) { word.translations.first.content}
     
       it { expect(subject).to be(true)}
+
+      it 'update good answers stats' do
+        expect { subject }.to change { game.reload.good_answers_count }.by(1)
+      end
     end
 
     context 'when bad answer is provided' do
       let(:answer) { 'Georgia'}
     
-      it { expect(subject).to be(false)}
+      it { expect(subject).to be(false) }
+
+      it 'update bad answers stats' do
+        expect { subject }.to change { game.reload.bad_answers_count }.by(1)
+      end
     end
   end
 end
