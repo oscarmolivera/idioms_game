e466abb6ec79ca5583011ab5f0086a6cf06d9ab5
Oscar M Olivera <oscarmolivera@gmail.com>
2020-07-13 17:57:11 -0400

    GameModel #Correctness close #65

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,17 @@
 class Game < ApplicationRecord
   belongs_to :user
+
+  def correctness
+    return '-' if total_answers.zero?
+
+    value = (good_answers_count.to_f / total_answers) * 100
+    value = value.round(2)
+    value.to_s + '%'
+  end
+
+  private
+
+  def total_answers
+    good_answers_count + bad_answers_count
+  end
 end
